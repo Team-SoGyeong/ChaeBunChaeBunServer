5d6252a429c32f52c608a48debc0dd1eadd9e2f1
dldbdud314 <dldbdud314@naver.com>
2021-08-01 12:52:14 +0900

    add settings

--- src/main/java/com/sogyeong/cbcb/CbcbApplication.java
+++ src/main/java/com/sogyeong/cbcb/CbcbApplication.java
@@ -2,12 +2,21 @@ package com.sogyeong.cbcb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CbcbApplication {
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "/Users/yooyounglee/Desktop/app/config/sogyeong-cbcb/real-application.yml";
 
 	public static void main(String[] args) {
-		SpringApplication.run(CbcbApplication.class, args);
+		new SpringApplicationBuilder(CbcbApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
+		//SpringApplication.run(CbcbApplication.class, args);
 	}
 
 }
