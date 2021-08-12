59dc07dfb8e864f97467f80cb6fa0174f30fa7a2
dldbdud314 <dldbdud314@naver.com>
2021-08-12 10:08:48 +0900

    [add]: 신고 기능 구현(향지 커밋 재업)

---
+++ src/main/java/com/sogyeong/cbcb/board/service/ReportService.java
@@ -0,0 +1,35 @@
+package com.sogyeong.cbcb.board.service;
+
+
+import com.sogyeong.cbcb.board.entity.Comment;
+import com.sogyeong.cbcb.board.model.ReportDTO;
+import com.sogyeong.cbcb.board.repository.PostsRepository;
+import com.sogyeong.cbcb.board.repository.ReportRepository;
+import com.sogyeong.cbcb.defaults.entity.Opinion;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ReportService {
+
+    private PostsRepository postsRepository;
+    private ReportRepository rRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Transactional(readOnly = false)
+    public Boolean storeReport(ReportDTO rDTO) {
+        int lastSeq= rRepository.getLastSeq(rDTO.getTyp());
+        Opinion oResult = rRepository.save(rDTO.toEntity());
+        if(oResult.getSeq()>lastSeq)
+            return true;
+        else return false;
+    }
+}
