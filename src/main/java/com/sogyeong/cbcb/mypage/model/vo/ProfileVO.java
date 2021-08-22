b3710292abd52c86651490b092b469a834a58600
dldbdud314 <dldbdud314@naver.com>
2021-08-22 19:50:18 +0900

    [add]: 프로필 조회, 수정 구현

---
+++ src/main/java/com/sogyeong/cbcb/mypage/model/vo/ProfileVO.java
@@ -0,0 +1,10 @@
+package com.sogyeong.cbcb.mypage.model.vo;
+
+import lombok.Data;
+
+@Data
+public class ProfileVO {
+    private long user_id;
+    private String profile_img;
+    private String nickname;
+}
--- src/main/java/com/sogyeong/cbcb/mypage/service/MyPageService.java
+++ src/main/java/com/sogyeong/cbcb/mypage/service/MyPageService.java
@@ -1,14 +1,31 @@
 package com.sogyeong.cbcb.mypage.service;
 
+import com.sogyeong.cbcb.mypage.entity.UserInfo;
+import com.sogyeong.cbcb.mypage.repository.UserInfoReposiorty;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class MyPageService {
     @PersistenceContext
     private EntityManager em;
+
+    UserInfoReposiorty userInfoReposiorty;
+
+    @Transactional
+    public Boolean updateProfile(long userId, String image, String nickname){
+        Optional<UserInfo> info = userInfoReposiorty.findById(userId);
+        info.ifPresent(profileInfo ->{
+            profileInfo.setUrl(image);
+            profileInfo.setNickname(nickname);
+            userInfoReposiorty.save(profileInfo);
+        });
+        return info.stream().count() > 0 ? true : false;
+    }
 }
