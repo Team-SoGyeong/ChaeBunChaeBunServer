bf852951c82822b7510520f2f065c0f97aa41a7a
sonhyangji <ths3437@naver.com>
2021-08-23 23:33:40 +0900

    [add]: 이미지변환 구현

---
+++ src/main/java/com/sogyeong/cbcb/config/S3Uploader.java
@@ -0,0 +1,71 @@
+package com.sogyeong.cbcb.config;
+
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.*;
+
+
+@Slf4j // 로깅을 위한 어노테이션
+@RequiredArgsConstructor // final 변수에 대한 의존성을 추가합니다.
+@Component // 빈 등록을 위한 어노테이션
+public class S3Uploader {
+
+    private final static String TEMP_FILE_PATH = "src/main/resources/";
+
+    private final AmazonS3Client amazonS3Client;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public List<String> upload(List<MultipartFile> multipartFile, String dirName) throws IOException {
+        List<String> uploadFiles =new ArrayList<>();
+        for(MultipartFile file : multipartFile) {
+            File uploadFile = convert(file)
+                    .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File로 전환이 실패했습니다."));
+            uploadFiles.add(upload(uploadFile, dirName));
+        }
+        return uploadFiles;
+    }
+
+    private String upload(File uploadFile, String dirName) {
+        String fileName = dirName + "/" + uploadFile.getName();
+        String uploadImageUrl = putS3(uploadFile, fileName);
+        removeNewFile(uploadFile);
+        return uploadImageUrl;
+    }
+
+    private String putS3(File uploadFile, String fileName) {
+        amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+    private void removeNewFile(File targetFile) {
+        if (targetFile.delete()) {
+            return;
+        }
+        log.info("임시 파일이 삭제 되지 못했습니다. 파일 이름: {}", targetFile.getName());
+    }
+
+    private Optional<File> convert(MultipartFile file) throws IOException {
+        File convertFile = new File(file.getOriginalFilename());
+        if(convertFile.createNewFile()) {
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            }
+            return Optional.of(convertFile);
+        }
+
+        return Optional.empty();
+    }
+
+}
---
+++ src/main/java/com/sogyeong/cbcb/config/UploadController.java
@@ -0,0 +1,23 @@
+package com.sogyeong.cbcb.config;
+
+import com.sogyeong.cbcb.defaults.entity.response.BasicResponse;
+import com.sogyeong.cbcb.defaults.entity.response.CommonResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class UploadController {
+
+    private final S3Uploader s3Uploader;
+
+    @PostMapping("/image/upload/{user}")
+    public ResponseEntity<? extends BasicResponse> upload(@RequestParam("data") List<MultipartFile> file, @PathVariable("user") long user) throws IOException {
+        return ResponseEntity.ok().body( new CommonResponse(s3Uploader.upload(file, "static/"+user),"이미지변환완료")); // S3 bucket의 static/ 폴더를 지정한 것.
+    }
+
+}
