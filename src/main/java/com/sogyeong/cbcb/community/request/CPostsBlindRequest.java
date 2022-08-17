1bcc562944a30688df159e0cfdc79ef430719929
dldbdud314 <dldbdud314@naver.com>
2022-08-17 10:11:13 +0900

    [fix] : report request swagger 추가

--- src/main/java/com/sogyeong/cbcb/community/request/CPostsBlindRequest.java
+++ src/main/java/com/sogyeong/cbcb/community/request/CPostsBlindRequest.java
@@ -1,5 +1,6 @@
 package com.sogyeong.cbcb.community.request;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,14 +13,18 @@ import javax.validation.constraints.NotNull;
 @Setter
 @ToString
 public class CPostsBlindRequest {
-    @NotNull
+    @NotNull @ApiModelProperty(value = "글 ID", required = true)
     private long post_id;
 
+    @ApiModelProperty(value = "댓글 ID (댓글 신고인 경우 추가)")
     private long cmt_id;
-    @NotNull
+
+    @NotNull @ApiModelProperty(value = "신고자 ID", required = true)
     private long author_id;
-    @NotNull
+
+    @NotNull @ApiModelProperty(value = "신고 사유 - 1: 광고성 글, 2: 부적절한 내용, 3: 기타", allowableValues = "1, 2, 3", required = true)
     private int reason_num;
 
+    @ApiModelProperty(value = "신고 사유 기타(3)인 경우 입력")
     private String reason;
 }
